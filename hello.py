0f84fdabf7c97d21f4df4f2e77daee6edbbb5d50
Eric Matthes <eric@Erics-MacBook-Pro.local>
2021-02-23 15:19:05 -0900

    Removed interactive feature.

--- hello.py
+++ hello.py
@@ -1,11 +1,8 @@
 import sys
 
-name = input("Hi, what's your name? ")
-
 print("Hello Git world!")
 print(f"Using Python version {sys.version}")
 
-print(f"Hello, {name.title()}!")
 print("We hope you like using Git.")
 
 
